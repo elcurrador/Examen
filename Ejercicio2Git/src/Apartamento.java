29d9ad5ca399f3b8b1a79386c99283e95d55bdb7
alumno <alumno@pc44>
2024-05-31 12:58:12 +0200

    Revert "commit Rama2"
    This reverts commit 9fc73ab879cc590097f36add88f796446da18774.

--- Ejercicio2Git/src/Apartamento.java
+++ Ejercicio2Git/src/Apartamento.java
@@ -1,15 +1,4 @@
-public class Apartamento extends InmuebleVivienda {
 
-	public Apartamento(int identificadorInmobiliario, int área, String dirección, int númeroHabitaciones,
-			int númeroBaños) {
-// Invoca al constructor de la clase padre
-		super(identificadorInmobiliario, área, dirección, númeroHabitaciones, númeroBaños);
-	}
+public class Apartamento {
 
-	/*
-	 * Método que muestra en pantalla los datos de un apartamento
-	 */
-	void imprimir() {
-		super.imprimir(); // Invoca al método imprimir de la clase padre
-	}
 }
