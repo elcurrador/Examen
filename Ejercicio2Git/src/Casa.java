ae8d7df56f21be55d5d171c9410a738d8acc3b5d
alumno <alumno@pc44>
2024-05-31 12:44:38 +0200

    r_especializacion creacion

--- Ejercicio2Git/src/Casa.java
+++ Ejercicio2Git/src/Casa.java
@@ -1,13 +1,24 @@
 public class Casa extends InmuebleVivienda {
-protected int númeroPisos; /* Atributo que identica el número de 
-pisos que tiene una casa */
+	protected int númeroPisos; /*
+								 * Atributo que identica el número de pisos que tiene una casa
+								 */
 
-public Casa(int identificadorInmobiliario, int área, String dirección, 
-int númeroHabitaciones, int númeroBaños, int númeroPisos) {
+	public Casa(int identificadorInmobiliario, int área, String dirección, int númeroHabitaciones, int númeroBaños,
+			int númeroPisos) {
 // Invoca al constructor de la clase padre
-super(identificadorInmobiliario, área, dirección, 
-númeroHabitaciones, númeroBaños);
-this.númeroPisos = númeroPisos;
-}
+		super(identificadorInmobiliario, área, dirección, númeroHabitaciones, númeroBaños);
+		this.númeroPisos = númeroPisos;
+	}
 
+	/*
+	 * 
+	 * Método que muestra en pantalla los datos de una casa
+	 * 
+	 */
+
+	void imprimir() {
+		super.imprimir();// Invoca al método imprimir de la clase padre
+
+		System.out.println("Número de pisos =" + númeroPisos);
+	}
 }
